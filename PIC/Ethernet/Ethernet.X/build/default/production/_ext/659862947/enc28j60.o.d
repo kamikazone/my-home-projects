22e1a8cdcc8562e4ac38be6bf4417aad318bb22a
rsrahul <rsrahul@gmail.com>
2011-10-01 07:03:53 +0000

    removed uart dependency still working

--- PIC/Ethernet/Ethernet.X/build/default/production/_ext/659862947/enc28j60.o.d
+++ PIC/Ethernet/Ethernet.X/build/default/production/_ext/659862947/enc28j60.o.d
@@ -4,5 +4,4 @@ build/default/production/_ext/659862947/enc28j60.o: ../src/net/enc28j60.c \
  C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/include/generic_typedefs.h \
  C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/net/include/helpers.h \
  C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/net/include/mac.h \
- C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/net/include/enc28j60.h \
- C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/uart/include/uart.h
+ C:/Users/Rahul/Documents/Projects/SVN/PIC/Ethernet/src/net/include/enc28j60.h
